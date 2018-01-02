3371dcc345218fd881a8223cd3cc51c604fc948f
Cheryl Frankenfield and Sailor Winkelman <sailor@>
2018-01-02 13:54:09 -0800

    Add user input logic to create currentPost from user input.

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -3,8 +3,11 @@ var Entry = require('./../js/scripts.js').entryModule;
 $(document).ready(function(){
   $("form#entry-form").submit(function() {
     event.preventDefault();
-    alert("jquery is working");
-    alert("gulp is watching development js and bower");
+    var title = $('input#entry-title').val();
+    var body = $('input#entry-body').val();
+
+    var currentPost = new Entry(title, body);
+    console.log(currentPost);
   });
 });
 
